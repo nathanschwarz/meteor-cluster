01dbc835fff9e9a98308615cf9c0188c91d78bc2
Nathan Schwarz <nathan.schwarz97@gmail.com>
2020-11-18 18:29:08 +0100

    updated package.js to export Cluster and TaskQueue classes

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'nschwarz:cluster',
   version: '1.0.0',
-  summary: 'Use modernizr in your meteor project',
+  summary: 'native nodejs clusterization for meteor server',
   git: 'https://github.com/nathanschwarz/meteor-cluster.git',
   documentation: 'README.md'
 })
@@ -17,4 +17,8 @@ Package.registerBuildPlugin({
 Package.onUse((api) => {
   api.versionsFrom('1.9')
   api.use('check')
+  api.addFiles('src/Cluster.js', 'server');
+  api.addFiles('src/TaskQueue.js', 'server');
+  api.export('TaskQueue', 'server');
+  api.export('Cluster', 'server');
 })
