6997bea4591756bc4a67140417be9ef7f19b5e59
Nathan Schwarz <nathan.schwarz97@gmail.com>
2020-11-19 20:37:14 +0100

    updated doc && version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'nschwarz:cluster',
-  version: '1.0.0',
+  version: '1.0.1',
   summary: 'native nodejs clusterization for meteor server',
   git: 'https://github.com/nathanschwarz/meteor-cluster.git',
   documentation: 'README.md'
