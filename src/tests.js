8e1a77d63f4f4b18a446a5aee47291b40a7c750f
Nathan Schwarz <nathan.schwarz97@gmail.com>
2021-01-20 14:51:40 +0100

    [tests]: added test for scheduled jobs

--- src/tests.js
+++ src/tests.js
@@ -11,9 +11,43 @@ const taskMap = {
 				console.log(`\n\n[simpleAsyncTest][${job._id}]: running succesfully\n\n`)
 			), 1000)
 		})
+	},
+	schedTest(job) {
+		const now = Date.now()
+		console.log(job)
+		const expectedDate = new Date(job.dueDate).getTime()
+		const createdAt = new Date(job.createdAt).getTime()
+		const wantedTime = expectedDate - createdAt
+		const msDiff = Math.abs(now - expectedDate)
+
+		if (msDiff < 0) {
+			throw new Error(`\n\n[schedTest][${job._id}]: called too soon: diff is ${msDiff}ms\n\n`)
+		} else if (msDiff > 1000) {
+			throw new Error(`\n\n[schedTest][${job._id}]: called too late: diff is ${msDiff}ms\n\n`)
+		}
+		console.log(`\n\n[schedTest][${job._id}]: called on time: diff is ${msDiff}ms\n\n`)
 	}
 }
 
+function inMemoryJobs() {
+	console.log('\n\n####### IN_MEMORY TESTS #######\n\n')
+	TaskQueue.addTask({ _id: 'IN_MEMORY_SYNC', priority: 1, taskType: 'simpleTest', data: { isLastJob: false }, inMemory: true })
+	TaskQueue.addTask({ _id: 'IN_MEMORY_ASYNC', priority: 1, taskType: 'simpleAsyncTest', data: { isLastJob: true }, inMemory: true })
+}
+
+function cleanup() {
+	console.log('\n\n####### CLEANING UP #######\n\n')
+	TaskQueue.remove({ _id: { $in: [ 'MONGO_SYNC', 'MONGO_ASYNC', 'SCHED' ]}})
+}
+
+function handleOtherEvents({ startsInMemory, isLastJob }) {
+	if (startsInMemory) {
+			inMemoryJobs()
+		} else if (isLastJob) {
+			cleanup()
+			console.log('\n\n####### TEST SUITE DONE #######\n\n')
+		}
+}
 function onJobDone(job) {
 	const task = job.task
 	const inMemory = task._id.startsWith('inMemory_')
@@ -24,24 +58,29 @@ function onJobDone(job) {
 	} else {
 		console.error(`[${task.taskType}][${task._id}]: still in queue after execution`)
 	}
-	if (task.data.startsInMemory) {
-		console.log('\n\n####### IN_MEMORY TESTS #######\n\n')
-		TaskQueue.addTask({ _id: 'IN_MEMORY_SYNC', priority: 1, taskType: 'simpleTest', data: { isLastJob: false }, inMemory: true })
-		TaskQueue.addTask({ _id: 'IN_MEMORY_ASYNC', priority: 1, taskType: 'simpleAsyncTest', data: { isLastJob: true }, inMemory: true })
-	} else if (task.data.isLastJob) {
-		console.log('\n\n####### TEST SUITE DONE #######\n\n')
-	}
+	handleOtherEvents(task.data)
 }
 
-const cluster = new Cluster(taskMap)
+function onJobError(job) {
+	const task = job.task
+	handleOtherEvents(task.data)
+}
+
+const cluster = new Cluster(taskMap, { refreshRate: 500 })
 
 function testSuite() {
 	if (Cluster.isMaster()) {
+		cleanup()
 		TaskQueue.addEventListener('done', onJobDone)
-		console.log('####### MONGO TASKS TESTS #######\n\n')
+		TaskQueue.addEventListener('error', onJobError)
+		console.log('\n\n####### MONGO TASKS TESTS #######\n\n')
 		TaskQueue.addTask({ _id: 'MONGO_SYNC', priority: 1, taskType: 'simpleTest', data: { isLastJob: false }})
-		TaskQueue.addTask({ _id: 'MONGO_ASYNC', priority: 1, taskType: 'simpleAsyncTest', data: { isLastJob: false, startsInMemory: true }})
+		TaskQueue.addTask({ _id: 'MONGO_ASYNC', priority: 1, taskType: 'simpleAsyncTest', data: { isLastJob: false }})
+		console.log('\n\n####### SCHED TEST #######\n\n')
+		const dueDate = new Date()
+		dueDate.setSeconds(dueDate.getSeconds() + 5)
+		TaskQueue.addTask({ _id: 'SCHED', priority: 1, taskType: 'schedTest', data: { isLastJob: false, startsInMemory: true }, dueDate })
 	}
 }
 
-testSuite()
+Meteor.startup(testSuite)
