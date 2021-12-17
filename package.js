6ea72117a5e110478bcb131d0bf1ff43bbc09639
Nathan Schwarz <nathan.schwarz97@gmail.com>
2021-12-17 10:58:29 +0100

    bumbed package version from 2.1.9 => 2.2.0

--- package.js
+++ package.js
@@ -1,23 +1,23 @@
 Package.describe({
   name: 'nschwarz:cluster',
-  version: '2.1.9',
+  version: '2.2.0',
   summary: 'native nodejs clusterization for meteor server',
   git: 'https://github.com/nathanschwarz/meteor-cluster.git',
-  documentation: 'README.md'
+  documentation: 'README.md',
 })
 
 Package.onUse(api => {
   api.versionsFrom('1.9')
-  api.use([ 'mongo', 'ecmascript', 'random' ])
+  api.use(['mongo', 'ecmascript', 'random'])
   api.mainModule('src/index.js', 'server')
 })
 
 Npm.depends({
-  debug: '4.3.1'
+  debug: '4.3.1',
 })
 
 Package.onTest(api => {
-	api.use('nschwarz:cluster')
-	api.use([ 'ecmascript' ])
-	api.mainModule('src/tests/_index.js')
+  api.use('nschwarz:cluster')
+  api.use(['ecmascript'])
+  api.mainModule('src/tests/_index.js')
 })
